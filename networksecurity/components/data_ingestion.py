50214596b66b2c1d1cf1e9db9b00b9085e543fd4
akshat-0921 <akshat21lko@gmail.com>
2025-06-09 11:02:39 +0530

    fixing exception issue

--- networksecurity/components/data_ingestion.py
+++ networksecurity/components/data_ingestion.py
@@ -42,7 +42,7 @@ class DataIngestion:
             df.replace({"na":np.nan},inplace=True)
             return df
         except Exception as e:
-            raise NetworkSecurityException
+            raise NetworkSecurityException(e,sys)
         
     def export_data_into_feature_store(self,dataframe: pd.DataFrame):
         try:
@@ -91,4 +91,4 @@ class DataIngestion:
             return dataingestionartifact
 
         except Exception as e:
-            raise NetworkSecurityException
+            raise NetworkSecurityException(e,sys)
